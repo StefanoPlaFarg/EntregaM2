969abcb77833956906880c7334ac2133aa15c6fb
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-23 13:02:06 +0100

    Branca: Fase3

--- src/FaseApp.java
+++ src/FaseApp.java
@@ -1,12 +1,42 @@
 
+//FASE
+//Emmagatzemar en un Map tant les lletres de la llista com el nombre de vegades que apareixen. 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FaseApp {
 
 	public static void main(String[] args) {
-		char[] nom ={'S','T','E','F','A','N','O'};
-		for (int i=0;i<nom.length;i++) {
-			System.out.println(nom[i]);
-		}
-
+		// TODO Auto-generated method stub
+		HashMap<Character, Integer> contenidor = new HashMap<Character, Integer>();
+		ArrayList<Character> nom = new ArrayList<>();
+	    nom.add('S');
+	    nom.add('T');
+	    nom.add('E');
+	    nom.add('F');
+	    nom.add('A');
+	    nom.add('N');
+	    nom.add('N');
+		
+	    
+	    for (Character lletra:nom) {
+	       	if (contenidor.containsKey(lletra)==false ||contenidor.isEmpty() ) {	    		
+	    		contenidor.put(lletra, 1);
+	    			    			    		
+	    	}
+	    	
+	    	else {
+	    		contenidor.put(lletra, contenidor.get(lletra)+1);
+	    	}	    		    	
+	    	
+	    }
+	    
+	    for (Character c: contenidor.keySet()) {	    	
+	    	System.out.println("key: " + c + " value: " + contenidor.get(c));
+	    }
+		
+		
 
 	}
 
