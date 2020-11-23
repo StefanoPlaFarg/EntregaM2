da7e88e576ddc892cd8f439afd3a5b28ab8581cf
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-23 12:59:08 +0100

    Branca master: Fase1

---
+++ src/FaseApp.java
@@ -0,0 +1,13 @@
+
+public class FaseApp {
+
+	public static void main(String[] args) {
+		char[] nom ={'S','T','E','F','A','N','O'};
+		for (int i=0;i<nom.length;i++) {
+			System.out.println(nom[i]);
+		}
+
+
+	}
+
+}
