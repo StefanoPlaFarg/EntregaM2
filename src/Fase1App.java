9e2df8607d8e4597033dda7003873649083b9475
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-23 12:40:49 +0100

    Branch_master: Fase1

---
+++ src/Fase1App.java
@@ -0,0 +1,12 @@
+
+public class Fase1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] nom ={'S','T','E','F','A','N','O'};
+		for (int i=0;i<nom.length;i++) {
+			System.out.println(nom[i]);
+		}
+	}
+}
+ 
